aa6e0de9fbdadbbd8e643e4789c038fd8a1df029
Maame-Yaa <90871297+mytwebsites@users.noreply.github.com>
2024-03-07 13:56:04 +0000

    changed new.py and added comments

--- chatbot/new.py
+++ chatbot/new.py
@@ -1,39 +1,41 @@
-# We bring in some code from other files that we need for our chatbot to work.
-import random
-import json
-import pickle
-import numpy as np
-import tensorflow as tf
+# This Python file sets up the basic components needed for a chatbot.
+# It prepares the data from 'intents.json' to understand and respond to user messages.
+# The chatbot will learn from examples provided in 'intents.json'.
 
-# nltk is a toolkit we use to work with language data.
-import nltk
-from nltk.stem import WordNetLemmatizer
+import random  # Used to generate random responses.
+import json  # Helps with reading and writing json files.
+import pickle  # Used to save and load Python objects to and from files.
+import numpy as np  # Helpful for numerical operations.
+import tensorflow as tf  # Provides tools for machine learning.
 
-# We create a 'lemmatizer' that helps us get the root form of words. 
-# A lemmatizer is a tool that changes words into their base or root form. For example, it changes "running" into "run"
-lemmatizer = WordNetLemmatizer()
+import nltk  # Natural Language Toolkit for processing human language.
+from nltk.stem import WordNetLemmatizer  # Helps get the root form of words.
 
-# We load our chatbot's 'intents' from a file which tells it how to reply to different things people might say.
+lemmatizer = WordNetLemmatizer()  # Create a tool to turn words into their base form.
+
+# Open the 'intents.json' file and read the content into a Python dictionary.
 intents = json.loads(open('intents.json').read())
 
-# Here we set up some lists to store different parts of chatbot's language understanding.
-words = []  # This will hold all individual words from patterns.
-classes = []  # This will hold all the types of things users might want to do, like asking for help or buying something.
-documents = []  # This will pair up patterns with intents.
-ignoredLetters = ['?', '!', '.', ',']  # These are characters we don't care about.
+# Lists to hold words, categories (classes), and pairs of patterns and categories.
+words = []
+classes = []
+documents = []
+ignoredLetters = ['?', '!', '.', ',']  # Punctuation marks to ignore.
 
-# We go through all our intents and each pattern in them to fill our lists above.
+# Go through each intent and process the patterns and tags.
 for intent in intents['intents']:
     for pattern in intent['patterns']:
-        # We break the pattern into individual words.
+        # Break each pattern into words.
         wordList = nltk.word_tokenize(pattern)
-        words.extend(wordList)  # Add the words from this pattern to our big list of words.
-        documents.append((wordList, intent['tag']))  #  creates a tuple of the tokenized pattern and its associated tag, then adds it to the documents list
+        words.extend(wordList)  # Add the words to our words list.
+        # Add a tuple of words and the tag to our documents list.
+        documents.append((wordList, intent['tag']))
+        # If the tag is not already in our classes list, add it.
         if intent['tag'] not in classes:
-            classes.append(intent['tag'])  # If the tag is not in classes, it gets added. classes will eventually contain all the unique tags 
+            classes.append(intent['tag'])
 
-# Now we make sure each word is in its root form and our lists only have unique items.
-words = [lemmatizer.lemmatize(word) for word in words if word not in ignoredLetters]  # Lemmatize words not in ignoredLetters.
-words = sorted(set(word))  # Sort the words and remove any duplicates.
+# Lemmatize the words and remove duplicates.
+words = [lemmatizer.lemmatize(word) for word in words if word not in ignoredLetters]
+words = sorted(set(words))  # Sort the words.
 
-classes = sorted(set(classes))  # Sort the classes and remove any duplicates.
+classes = sorted(set(classes))  # Remove duplicates and sort the classes.
